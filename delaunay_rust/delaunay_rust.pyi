8866dd45ca56656c8a74e83c676bfc7527b0d321
Hubert Mysliwiec <hubernatorpro@gmail.com>
2026-06-03 17:40:13 +0200

    add pyi file

---
+++ delaunay_rust/delaunay_rust.pyi
@@ -0,0 +1,16 @@
+import numpy as np
+from typing import List, Tuple
+
+
+def triangulate_points(
+        points_array: np.ndarray,
+        use_triangle_walk: bool = True
+) -> List[Tuple[int, int, int]]:
+    """Oto twój docstring ziomek, który będzie ładnie widoczny w edytorze."""
+    ...
+
+
+def triangulate_points_dnc(
+        points_array: np.ndarray
+) -> List[Tuple[int, int, int]]:
+    ...
